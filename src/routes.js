f2db51e31bc3419edf9e54ff2b7ab1282ed26254
sakhila-yunara <123307871+sakhila-yunara@users.noreply.github.com>
2023-07-28 13:32:52 +0700

    Update routes.js

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: './books',
+    handler: addBookHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: updateBookByIdHandler,
+  },
+
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/',
+    handler: getAllBooksHandler,
+  },
+
+];
+
+module.exports = routes;
